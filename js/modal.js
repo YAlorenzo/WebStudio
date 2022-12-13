83c30e679ec79351e45b2bb45a399bf1247a5f91
YAlorenzo <zhenia.klimenchuk@gmail.com>
2022-12-13 17:09:53 +0100

    version_1

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+  }
+})();
+$(document).ready(function(){
+	$('order-section__button').click(function(){
+		$(this).toggleClass('open');
+  });
+  
+  
+});
+  
+
+
+
+
